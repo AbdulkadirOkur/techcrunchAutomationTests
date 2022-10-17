a44e20551c45be41b85bc114eba7a13408075c7c
Abdulkadir <abdulkadir.kangal@gmail.com>
2022-10-17 12:19:18 +0300

    CukesRunner class created for triggering the tests

--- src/test/java/com/techcrunch/runners/CukesRunner.java
+++ src/test/java/com/techcrunch/runners/CukesRunner.java
@@ -12,7 +12,7 @@ import org.junit.runner.RunWith;
         features = "src/test/resources/features",
         glue = "com/techcrunch/stepDefinitions",
         dryRun = false,
-        tags = "@wip"
+        tags = ""
 )
 public class CukesRunner {
 }
