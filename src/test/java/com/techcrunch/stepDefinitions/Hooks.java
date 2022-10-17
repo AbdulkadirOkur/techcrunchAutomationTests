e04c141e08a806aca3f2a2323ef729cf85b0a41d
Abdulkadir <abdulkadir.kangal@gmail.com>
2022-10-17 12:22:51 +0300

    Hooks class added for @After annotation use

--- src/test/java/com/techcrunch/stepDefinitions/Hooks.java
+++ src/test/java/com/techcrunch/stepDefinitions/Hooks.java
@@ -3,11 +3,10 @@ package com.techcrunch.stepDefinitions;
 import com.techcrunch.utils.Driver;
 import io.cucumber.java.After;
 import io.cucumber.java.Scenario;
-import org.openqa.selenium.OutputType;
-import org.openqa.selenium.TakesScreenshot;
 
 public class Hooks {
 
+    //This is for closing the driver after every test
     @After
     public void teardownScenario(Scenario scenario){
         Driver.closeDriver();
